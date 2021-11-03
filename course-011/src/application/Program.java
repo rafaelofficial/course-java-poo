3665f0b6cd6f3de031333e67d02cb9f946c0360b
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-11-02 21:14:54 -0300

    Update

--- course-011/src/application/Program.java
+++ course-011/src/application/Program.java
@@ -9,23 +9,39 @@ public class Program {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		
+		method1();
+
+		System.out.println("End of program!");
+		
+
+	}
+	
+	public static void method1() {
+		System.out.println("****METHOD1 START****");
+		method2();
+		System.out.println("****METHOD1 END****");
+	}
+
+	public static void method2() {
+		System.out.println("****METHOD2 START****");
+		Scanner sc = new Scanner(System.in);
+
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
-		}
-		catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Position invalid!");
-		}
-		catch (InputMismatchException e) {
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
 			System.out.println("Input error!");
+			e.printStackTrace();
+			sc.next();
 		}
-		
-		System.out.println("End of program!");
 		sc.close();
-
+		System.out.println("****METHOD2 END****");
 	}
 
 }
