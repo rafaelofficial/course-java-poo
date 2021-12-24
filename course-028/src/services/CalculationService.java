6089fc14f1f8d4c6e30377e886f8fdde88442e4e
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-24 00:14:32 -0300

    Solution with Generics - Product

--- course-028/src/services/CalculationService.java
+++ course-028/src/services/CalculationService.java
@@ -8,19 +8,17 @@ public class CalculationService {
 	 * @param list passa a lista que irá buscar o maior item dela  
 	 * @return retorna o maior item da lista
 	 */
-	public static Integer max(List<Integer> list) {
+	public static <T extends Comparable<T>> T max(List<T> list) {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List can't be empty");
 		}
 		
-		Integer max = list.get(0);
-		for (Integer item : list) {
+		T max = list.get(0);
+		for (T item : list) {
 			if (item.compareTo(max) > 0) {
 				max = item;
 			}
 		}	
 		return max;
 	}
-	
-
 }
