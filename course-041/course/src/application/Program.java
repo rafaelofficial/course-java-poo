23be5babaeeffccff01a238017909b17f536f098
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-29 16:14:12 -0300

    Experience with Comparator - Part 3
    Outra forma de ordenação utilizando o Comparator

--- course-041/course/src/application/Program.java
+++ course-041/course/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -18,7 +19,17 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+			/**
+			 * Compara a partir do nome, ignorando letras maiusculas e minusculas
+			 */
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		
+		list.sort(comp);
 		
 		for (Product products : list) {
 			System.out.println(products);
