27a51229bf78858fecebeaf7e12c89c63d3aaae8
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-29 16:24:15 -0300

    Experience with Comparator - Part 5 (Refactoring 1)

--- course-041/course/src/application/Program.java
+++ course-041/course/src/application/Program.java
@@ -22,9 +22,7 @@ public class Program {
 		/**
 		 * Compara a partir do nome, ignorando letras maiusculas e minusculas
 		 */
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		
 		list.sort(comp);
 		
