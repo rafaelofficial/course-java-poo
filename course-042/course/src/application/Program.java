fda74ba36fde2533529009fe32bfdf53c6a7c1fc
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-29 20:11:57 -0300

    Predicate - Part 4 (declared lambda expression)

--- course-042/course/src/application/Program.java
+++ course-042/course/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
-import utils.ProductPredicate;
 
 public class Program {
 
@@ -20,8 +20,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// Reference method com método não estático
-		list.removeIf(Product::nonStaticProductPredicate);
+		double min = 100.0;
+		// Expressão lambda declarada
+		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		list.removeIf(pred);
 		
 		for (Product p : list) {
 			System.out.println(p);
