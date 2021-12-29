787906f7418c6b7afdbc5482e24d3a141c7a1b3d
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-29 19:41:10 -0300

    Predicate - Part 1

---
+++ course-042/course/src/utils/ProductPredicate.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductPredicate implements Predicate<Product> {
+
+	@Override
+	public boolean test(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+}
