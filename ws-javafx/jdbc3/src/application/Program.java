70a1787bc0d5c0a5c29f35ca895f82da135b403b
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-01-03 22:00:19 -0300

    JDBC - Demo - Enter data (Part 1)

---
+++ ws-javafx/jdbc3/src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		/**
+		 * TODO: 
+		 * 1 - Conectar banco de dados
+		 * 2 - Instanciar o preparedStatement (insersao SQL)	
+		 * 3 - Instanciar os valores no preparedStatement (montar os comandos SQL (atribuir os valores para cada (?) ))
+		 * 4 - Executar os comandos com o excuteUpdate()
+		 * 5 - Fechar conexoes
+		 */
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"INSERT INTO seller " 
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)");
+			
+			st.setString(1, "Carl Puple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}	
+}
