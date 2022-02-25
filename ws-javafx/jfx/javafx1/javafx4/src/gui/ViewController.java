95ea17d84302610fe1f7663af5467623eb0b4c11
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-25 19:42:15 -0300

    Usando TextField e Label (app para calcular soma)

--- ws-javafx/jfx/javafx1/javafx4/src/gui/ViewController.java
+++ ws-javafx/jfx/javafx1/javafx4/src/gui/ViewController.java
@@ -2,16 +2,35 @@ package gui;
 
 import gui.util.Alerts;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
 	@FXML
-	private Button btnTest;
-	
+	private TextField txtNumberOne;
+
 	@FXML
-	public void btnTestAction() {
-		Alerts.showAlert("Alert title", null, "Hello World!", AlertType.INFORMATION);
+	private TextField txtNumberTwo;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btnSum;
+
+	@FXML
+	public void btnSumAction() {
+		try {
+			double numberOne = Double.parseDouble(txtNumberOne.getText());
+			double numberTwo = Double.parseDouble(txtNumberTwo.getText());
+			double sum = numberOne + numberTwo;
+
+			labelResult.setText(String.format("%.2f", sum));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Parse error", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
